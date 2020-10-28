a576557bf1da85034a0518f50126499298e25f2f
fd75 <dijacky>
2020-10-28 18:55:31 +0800

    demo

---
+++ security/rbac/src/main/java/org/paasplatform/security/rbac/jwt/RestAuthenticationFailureHandler.java
@@ -0,0 +1,51 @@
+package org.paasplatform.security.rbac.jwt;
+
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
+
+@Component("authenticationFailureHandler")
+public class RestAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Autowired
+    private MessageSource messages;
+
+    @Autowired
+    private LocaleResolver localeResolver;
+
+    @Override
+    public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException exception) throws IOException, ServletException {
+
+        final Locale locale = localeResolver.resolveLocale(request);
+
+        String errorMessage = messages.getMessage("message.badCredentials", null, locale);
+
+        if (exception instanceof DisabledException) {
+            errorMessage = messages.getMessage("auth.message.disabled", null, locale);
+        } else if (exception instanceof AccountExpiredException) {
+            errorMessage = messages.getMessage("auth.message.expired", null, locale);
+        } else if (exception instanceof LockedException) {
+            errorMessage = messages.getMessage("auth.message.blocked", null, locale);
+        } else if (exception.getMessage().equalsIgnoreCase("unusual location")) {
+            errorMessage = messages.getMessage("auth.message.unusual.location", null, locale);
+        }
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, errorMessage);
+    }
+}
---
+++ security/rbac/src/main/java/org/paasplatform/security/rbac/jwt/SpringWebSecurityConfigurer.java
@@ -0,0 +1,101 @@
+package org.paasplatform.security.rbac.jwt;
+
+import org.paasplatform.security.rbac.jpa.CustomAuthenticationProvider;
+import org.paasplatform.security.rbac.jpa.CustomWebAuthenticationDetailsSource;
+import org.paasplatform.security.rbac.jpa.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.PrintWriter;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SpringWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+   /* @Autowired
+    private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private LogoutSuccessHandler myLogoutSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler authenticationFailureHandler;
+*/
+    @Autowired
+    private CustomWebAuthenticationDetailsSource authenticationDetailsSource;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        final CustomAuthenticationProvider authProvider = new CustomAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        //authProvider.setPostAuthenticationChecks(differentLocationChecker);
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 开启登录配置
+        http.authorizeRequests()
+            .antMatchers("/authentication/*", "/login").permitAll()
+            .antMatchers("/user/**").hasAnyAuthority("read")// 需要具有ROLE_USER角色才能访问
+            .antMatchers("/admin/**").hasAnyAuthority("WRITE_PRIVILEGE") // 需要具有ROLE_ADMIN角色才能访问
+            .anyRequest()
+            .authenticated();
+
+        // Disable CSRF (cross site request forgery)
+        http.csrf().disable();
+
+        // No session will be created or used by spring security
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Apply JWT
+        http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+    }
+
+    /**
+     * Spring Security ignores URLs of static resources
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
